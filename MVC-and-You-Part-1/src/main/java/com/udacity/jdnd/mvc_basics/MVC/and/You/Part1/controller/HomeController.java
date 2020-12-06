05b0d788b198b18ccc26f951d8c13f8de8f00db0
yongjing <near711@outlook.com>
2020-12-06 13:54:06 +0800

    Model attribute added

--- MVC-and-You-Part-1/src/main/java/com/udacity/jdnd/mvc_basics/MVC/and/You/Part1/controller/HomeController.java
+++ MVC-and-You-Part-1/src/main/java/com/udacity/jdnd/mvc_basics/MVC/and/You/Part1/controller/HomeController.java
@@ -1,12 +1,16 @@
 package com.udacity.jdnd.mvc_basics.MVC.and.You.Part1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.Instant;
 
 @Controller
 public class HomeController {
     @RequestMapping("/home")
-    public String getHomePage () {
+    public String getHomePage (Model model) {
+        model.addAttribute("welcomeMessage", Instant.now().toString());
         return "home";
     }
 }
