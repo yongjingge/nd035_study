ef7379c9d5054b499ce7e7e14da9854339fee543
yongjing <near711@outlook.com>
2021-01-08 17:57:35 +0800

    lombok used

--- EntityEx/src/main/java/com/udacity/nd035/c3/EntityEx/ex1/data/delivery/Delivery.java
+++ EntityEx/src/main/java/com/udacity/nd035/c3/EntityEx/ex1/data/delivery/Delivery.java
@@ -1,5 +1,7 @@
 package com.udacity.nd035.c3.EntityEx.ex1.data.delivery;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.Nationalized;
 import org.hibernate.annotations.Type;
 
@@ -18,43 +20,47 @@ public class Delivery {
     Long id;
 
     @Nationalized
+    @Getter @Setter
     private String recipientName;
     @Column(name = "address_full", length = 500)
+    @Getter @Setter
     private String address;
     // private LocalDate deliveryDate;
+    @Getter @Setter
     private LocalDateTime deliveryTime; // include both Date and Time, no need to add a separated Date field
     @Type(type = "yes_no")
+    @Getter @Setter
     private boolean deliveryCompleted;
 
-    public String getRecipientName() {
-        return recipientName;
-    }
-
-    public void setRecipientName(String recipientName) {
-        this.recipientName = recipientName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public LocalDateTime getDeliveryTime() {
-        return deliveryTime;
-    }
-
-    public void setDeliveryTime(LocalDateTime deliveryTime) {
-        this.deliveryTime = deliveryTime;
-    }
-
-    public boolean isDeliveryCompleted() {
-        return deliveryCompleted;
-    }
-
-    public void setDeliveryCompleted(boolean deliveryCompleted) {
-        this.deliveryCompleted = deliveryCompleted;
-    }
+//    public String getRecipientName() {
+//        return recipientName;
+//    }
+//
+//    public void setRecipientName(String recipientName) {
+//        this.recipientName = recipientName;
+//    }
+//
+//    public String getAddress() {
+//        return address;
+//    }
+//
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
+//
+//    public LocalDateTime getDeliveryTime() {
+//        return deliveryTime;
+//    }
+//
+//    public void setDeliveryTime(LocalDateTime deliveryTime) {
+//        this.deliveryTime = deliveryTime;
+//    }
+//
+//    public boolean isDeliveryCompleted() {
+//        return deliveryCompleted;
+//    }
+//
+//    public void setDeliveryCompleted(boolean deliveryCompleted) {
+//        this.deliveryCompleted = deliveryCompleted;
+//    }
 }
--- EntityEx/src/main/java/com/udacity/nd035/c3/EntityEx/ex1/data/inventory/Flower.java
+++ EntityEx/src/main/java/com/udacity/nd035/c3/EntityEx/ex1/data/inventory/Flower.java
@@ -1,5 +1,7 @@
 package com.udacity.nd035.c3.EntityEx.ex1.data.inventory;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.Nationalized;
 
 import javax.persistence.*;
@@ -14,32 +16,35 @@ public class Flower {
     Long id;
 
     @Nationalized
+    @Getter @Setter
     private String name;
+    @Getter @Setter
     private String color;
     @Column(name = "price", precision = 12, scale = 4)
+    @Getter @Setter
     private BigDecimal price;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getColor() {
+//        return color;
+//    }
+//
+//    public void setColor(String color) {
+//        this.color = color;
+//    }
+//
+//    public BigDecimal getPrice() {
+//        return price;
+//    }
+//
+//    public void setPrice(BigDecimal price) {
+//        this.price = price;
+//    }
 }
