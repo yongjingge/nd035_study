1f5a193cf33f57197fdb57723ed9762f4cf83043
yongjing <near711@outlook.com>
2021-01-13 18:43:58 +0800

    Data Sources Exercise 1

---
+++ EntityEx/src/main/resources/plant.sql
@@ -0,0 +1,3 @@
+create schema `plant`;
+create user 'sa'@'localhost' identified by 'sa1234';
+grant all on plant.* to 'sa'@'localhost';
---
+++ EntityEx/src/test/java/com/udacity/nd035/c3/EntityEx/EntityExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.nd035.c3.EntityEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
