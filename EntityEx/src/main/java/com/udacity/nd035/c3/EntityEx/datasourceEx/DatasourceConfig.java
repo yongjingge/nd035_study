af8137de2f8f147a55a0631e86ecdaeb2b796536
yongjing <near711@outlook.com>
2021-01-13 21:41:08 +0800

    Data Sources Exercise 2

---
+++ EntityEx/src/main/java/com/udacity/nd035/c3/EntityEx/datasourceEx/DatasourceConfig.java
@@ -0,0 +1,29 @@
+package com.udacity.nd035.c3.EntityEx.datasourceEx;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatasourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("com.udacity.datasource")
+    public DataSourceProperties getDataSourceProperties () {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "com.udacity.datasource.configuration")
+    public DataSource getDataSource (DataSourceProperties dataSourceProperties) {
+        return dataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+    }
+}
